c8c3585bbbf1f755ec76fb8e9f863ea596c38688
Marco Tabasco <matabasco@gmail.com>
2023-11-08 13:46:30 +0100

    Incentive program - Deployment script (#6)
    * distribution contract deployment script
    
    * use fetch, change script run command

---
+++ incentivation_program/README.md
@@ -0,0 +1,47 @@
+# SSV Rewards distributor contract deployment script
+This script allows users to fetch, compile, and deploy the 1inch CumulativeMerkleDrop contract from its [github repository](https://github.com/1inch/merkle-distribution/tree/1f8b2a6ed27d1b2d18cf8475e42eece60f41c896) to an Ethereum network.
+
+## Prerequisites
+Node.js
+npm
+
+## Installation
+Clone this repository:
+```bash
+git clone [REPOSITORY_LINK]
+cd [REPOSITORY_DIRECTORY]
+```
+
+Install the required dependencies:
+```bash
+npm install
+```
+
+## Usage
+To deploy the contract, run:
+
+```bash
+npm run deploy-ssv-distributor -- -k [OWNER_PRIVATE_KEY] -u [PROVIDER_URL] -t [SSV_TOKEN_ADDRESS]
+```
+
+Replace [OWNER_PRIVATE_KEY], [PROVIDER_URL], and [SSV_TOKEN_ADDRESS] with the private key of owner of the contract, provider URL, and the SSV token address, respectively.
+
+Options:
+* -k, --privateKey <string>: Your Ethereum private key.
+* -u, --providerUrl <string>: Ethereum provider URL (e.g., http://localhost:8545).
+* -t, --tokenAddress <string>: SSV token address.
+
+## Contract Details
+The script fetches and compiles the 1inch [CumulativeMerkleDrop]((https://github.com/1inch/merkle-distribution/blob/1f8b2a6ed27d1b2d18cf8475e42eece60f41c896/contracts/CumulativeMerkleDrop.sol)) contract at this specific [commit](https://github.com/1inch/merkle-distribution/tree/1f8b2a6ed27d1b2d18cf8475e42eece60f41c896) and its associated interface [ICumulativeMerkleDrop](https://github.com/1inch/merkle-distribution/blob/1f8b2a6ed27d1b2d18cf8475e42eece60f41c896/contracts/interfaces/ICumulativeMerkleDrop.sol) from the 1inch merkle-distribution GitHub repository. The contract is then deployed to the specified Ethereum network using the provided private key.
+
+## Dependencies
+* solc: Solidity compiler for compiling the contract.
+* ethers: Ethereum library for contract deployment and interaction.
+* commander: For parsing command-line arguments.
+
+## TODOs
+ - [ ] NPM private package, so that users don't have to clone the repository.
+ - [ ] Improve logging (output to file, etc.)
+ - [ ] Auto verification of the deployed contract on Etherscan.
+ - [ ] Add unit tests.
+ - [ ] CHANGELOG.md
---
+++ incentivation_program/index.js
@@ -0,0 +1,104 @@
+#!/usr/bin/env node
+const solc = require('solc');
+const { ethers } = require('ethers');
+const program = require('commander');
+const fs = require('fs')
+const path = require('path')
+
+const MAIN_CONTRACT_URL = "https://raw.githubusercontent.com/1inch/merkle-distribution/1f8b2a6ed27d1b2d18cf8475e42eece60f41c896/contracts/CumulativeMerkleDrop.sol";
+const INTERFACE_URL = "https://raw.githubusercontent.com/1inch/merkle-distribution/1f8b2a6ed27d1b2d18cf8475e42eece60f41c896/contracts/interfaces/ICumulativeMerkleDrop.sol";
+const CONTRACT_NAME = 'CumulativeMerkleDrop';
+
+async function fetchContentFromURL(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.text();
+}
+
+async function compileContract(contractContent, interfaceContent) {
+    const openzeppelinPath = path.resolve(__dirname, 'node_modules', '@openzeppelin', 'contracts');
+
+    const input = {
+        language: 'Solidity',
+        sources: {
+            'CumulativeMerkleDrop.sol': {
+                content: contractContent
+            },
+            'interfaces/ICumulativeMerkleDrop.sol': {
+                content: interfaceContent
+            },
+            '@openzeppelin/contracts/access/Ownable.sol': {
+                content: fs.readFileSync(path.resolve(openzeppelinPath, 'access', 'Ownable.sol'), 'utf8')
+            },
+            '@openzeppelin/contracts/token/ERC20/ERC20.sol': {
+                content: fs.readFileSync(path.resolve(openzeppelinPath, 'token', 'ERC20', 'ERC20.sol'), 'utf8')
+            },
+            '@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol': {
+                content: fs.readFileSync(path.resolve(openzeppelinPath, 'token', 'ERC20', 'extensions', 'IERC20Metadata.sol'), 'utf8')
+            },
+            '@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol': {
+                content: fs.readFileSync(path.resolve(openzeppelinPath, 'token', 'ERC20', 'extensions', 'IERC20Permit.sol'), 'utf8')
+            },
+            '@openzeppelin/contracts/token/ERC20/IERC20.sol': {
+                content: fs.readFileSync(path.resolve(openzeppelinPath, 'token', 'ERC20', 'IERC20.sol'), 'utf8')
+            },
+            '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol': {
+                content: fs.readFileSync(path.resolve(openzeppelinPath, 'token', 'ERC20', 'utils', 'SafeERC20.sol'), 'utf8')
+            },
+            '@openzeppelin/contracts/utils/Address.sol': {
+                content: fs.readFileSync(path.resolve(openzeppelinPath, 'utils', 'Address.sol'), 'utf8')
+            },
+            '@openzeppelin/contracts/utils/Context.sol': {
+                content: fs.readFileSync(path.resolve(openzeppelinPath, 'utils', 'Context.sol'), 'utf8')
+            },
+        },
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 10000
+            },
+            outputSelection: {
+                '*': {
+                    '*': ['*']
+                }
+            }
+        }
+    };
+
+    const output = JSON.parse(solc.compile(JSON.stringify(input)));
+    if (output.errors) {
+        console.error(output.errors);
+        throw new Error('Compilation failed');
+    }
+
+    return {
+        abi: output.contracts[`${CONTRACT_NAME}.sol`][CONTRACT_NAME].abi,
+        bytecode: output.contracts[`${CONTRACT_NAME}.sol`][CONTRACT_NAME].evm.bytecode.object
+    };
+}
+
+async function deployContract(args) {
+    const contractContent = await fetchContentFromURL(MAIN_CONTRACT_URL);
+    const interfaceContent = await fetchContentFromURL(INTERFACE_URL);
+    const { abi, bytecode } = await compileContract(contractContent, interfaceContent, CONTRACT_NAME);
+
+    const provider = new ethers.getDefaultProvider(args.providerUrl);
+    const wallet = new ethers.Wallet(args.privateKey, provider);
+
+    const contractFactory = new ethers.ContractFactory(abi, bytecode, wallet);
+    const contract = await contractFactory.deploy(args.tokenAddress);
+    await contract.deployed();
+
+    console.log(`Contract deployed to: ${contract.address}`);
+}
+
+program
+    .version('1.0.0')
+    .requiredOption('-k, --privateKey <string>', 'Account private key')
+    .requiredOption('-u, --providerUrl <string>', 'Ethereum provider URL (e.g., http://localhost:8545)')
+    .requiredOption('-t, --tokenAddress <string>', 'SSV token address')
+    .action(deployContract);
+
+program.parse(process.argv);
